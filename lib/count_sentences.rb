335493daae27a4e4b759ed11b00d43c1fdb90921
julianxlozano <julianxlozano@gmail.com>
2021-03-01 11:17:43 -0600

    commit tree 1cce4dba178d903fe8537d3fe71a01ad89c68e6e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+  
+    chomped=self.chomp
+ 
+    split=chomped.split(/[.!?]/)
+    #binding.pry 
+    split.delete_if(&:empty?).count 
+
 
   end
 end
